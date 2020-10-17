92d9a4b140fd8a1d532fa659bf53712a73795143
micovs <stojancomicov@gmail.com>
2020-10-17 03:27:40 -0400

    Metadata table created and options menu dropdown

--- static/js/app.js
+++ static/js/app.js
@@ -1,28 +1,51 @@
+
+// Function to get the data and build a options menu for the different IDs
+function BuildOptionMenu() {
+  d3.json("./data/samples.json").then(function(data) {
+    var names = data.names;
+    var select = d3.select("#selDataset");
+    // For each Id in the names array, creates a option tag and adds the ID as a text
+    names.forEach(id => {
+      var option = select.append("option");
+      option.text(id);
+    })
+  });
+};
+BuildOptionMenu()
+
+
+
+
+
+
 function geData() {
     d3.json("./data/samples.json").then(function(x) {
       var data = x;
       console.log(data);
       var samples = data.samples;
-      console.log(samples);
+      
       var dataId940 = samples.filter(sample =>sample.id == 940);
-      console.log(dataId940);
+      // console.log(dataId940);
       var Ids = dataId940[0].otu_ids;
       var Ids10 = dataId940[0].otu_ids.slice(0,10);
-      console.log(Ids);
+      // console.log(Ids);
       var IdsAsString = Ids10.map(Id => "OTU "+Id);
-      console.log(IdsAsString);
+      // console.log(IdsAsString);
       var sample_value = dataId940[0].sample_values;
       var sample_value10 = dataId940[0].sample_values.slice(0,10).reverse();
-      console.log(sample_value);
+      // console.log(sample_value);
       var sample_labels = dataId940[0].otu_labels;
       var sample_labels10 = dataId940[0].otu_labels.slice(0,10);
-      console.log(sample_labels);
+      // console.log(sample_labels);
+
+      var mData = data.metadata.filter( d => d.id == 940)[0];
+      console.log(mData)
 
     buildBarChart(IdsAsString, sample_value10, sample_labels10);
 
     BuildBubbleChart(Ids, sample_value, sample_labels);
 
-
+    buildMDataPanel(mData);
 
     });
 };
@@ -93,3 +116,12 @@ var layout = {
 Plotly.newPlot("bubble", data, layout);
 
 };
+
+function buildMDataPanel (mData){
+  var div = d3.select("#sample-metadata");
+  Object.entries(mData).forEach(([key, value]) => {
+    var p = div.append("p");
+    p.text(`${key}: ${value}`);
+  });
+};
+
